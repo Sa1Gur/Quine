b7f2a768a92a08c703676fdd6a222bc21ef4474c
Guriy Samarin <guriysamarin@outlook.com>
2022-01-07 13:12:24 +0300

    first version (simple one)

--- Quine.Test/QuineTest.cs
+++ Quine.Test/QuineTest.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Quine.Test;
@@ -5,7 +8,18 @@ namespace Quine.Test;
 public class QuineTest
 {
     [Fact]
-    public void Test1()
+    public async Task Test_Is_It_Quine()
     {
+        var code = await File.ReadAllTextAsync("../../Quine/Program.cs");
+        Process quineExe = new()
+        {
+            StartInfo = new ("Quine.exe")
+            {
+                RedirectStandardOutput = true
+            }
+        };
+        quineExe.Start();
+        var output = await quineExe.StandardOutput.ReadToEndAsync();
+        Assert.Equal(code, output);
     }
 }
--- Quine/Program.cs
+++ Quine/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var str = @"var str = @""
+
+Console.WriteLine(str[..12] + str[..12] + str[11] + str[12..] + str[11] + str[^1] + str[12..]);";
+
+Console.WriteLine(str[..12] + str[..12] + str[11] + str[12..] + str[11] + str[^1] + str[12..]);
