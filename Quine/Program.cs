42b3443cbe2b56ae0f9802391639bd2a8679e6ff
Guriy Samarin <guriysamarin@outlook.com>
2022-01-11 13:21:47 +0300

    added variable with correct recalculation

--- Quine/Program.cs
+++ Quine/Program.cs
@@ -1,5 +1,31 @@
-﻿var str = @"var str = @""
+﻿var _2blQuotePos = 38;
 
-Console.WriteLine(str[..12] + str[..12] + str[11] + str[12..] + str[11] + str[^1] + str[12..]);";
+var str = @$"var {nameof(_2blQuotePos)} = {_2blQuotePos};
 
-Console.WriteLine(str[..12] + str[..12] + str[11] + str[12..] + str[11] + str[^1] + str[12..]);
+var str = @$""
+
+Console.WriteLine(
+    str[..(_2blQuotePos + 1)] +
+    str[..4] + str[^2] + str[^8..^2] + str[60] + str[4..16] + str[^10] + str[^1] +
+    str[(_2blQuotePos - 5)..(_2blQuotePos - 2)] + 
+    str[^2] + str[4..16] + str[^1] + 
+    str[21..(_2blQuotePos + 1)] +
+    str[_2blQuotePos..^8] +
+    str[^8..^1] + str[^2] + str[^1] + str[^1] +
+    str[_2blQuotePos] +
+    str[^9] +
+    str[(_2blQuotePos + 1)..^8]
+    );nameof{{}}";
+
+Console.WriteLine(
+    str[..(_2blQuotePos + 1)] +
+    str[..4] + str[^2] + str[^8..^2] + str[60] + str[4..16] + str[^10] + str[^1] +
+    str[(_2blQuotePos - 5)..(_2blQuotePos - 2)] + 
+    str[^2] + str[4..16] + str[^1] + 
+    str[21..(_2blQuotePos + 1)] +
+    str[_2blQuotePos..^8] +
+    str[^8..^1] + str[^2] + str[^1] + str[^1] +
+    str[_2blQuotePos] +
+    str[^9] +
+    str[(_2blQuotePos + 1)..^8]
+    );
